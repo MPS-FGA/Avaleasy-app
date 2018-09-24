aa6d29ce498d20d7a6109f728707a504940c8b4c
IagoCarvalho <IagoVC_2012@hotmail.com>
2018-09-24 17:11:49 -0300

    Adding Teacher creation Form
    Co-authored-by: Matheus <matheusbattista@hotmail.com>

--- App.js
+++ App.js
@@ -1,23 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { TeacherForm } from './src/components/teachers_form.js'
+import t from 'tcomb-form-native';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
+const Form =t.form.Form;
+
+const Teacher = t.struct({
+  email: t.String,
+  name: t.String,
+  password: t.String,
+});
+
+const formStyles = {
+  ...Form.stylesheet,
+  formGroup: {
+    normal: {
+      marginBottom: 10
+    },
+  },
+  controlLabel: {
+    normal: {
+      color: 'blue',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    },
+    // the style applied when a validation error occours
+    error: {
+      color: 'red',
+      fontSize: 18,
+      marginBottom: 7,
+      fontWeight: '600'
+    }
   }
+}
+
+const options = {
+  fields: {
+    email: {
+      error: 'Without an email address how are you going to reset your password when you forget it?'
+    },
+    password: {
+      error: 'Choose something you use on a dozen other sites or something you won\'t remember'
+    },
+  },
+  stylesheet: formStyles,
+};
+
+
+export default class App extends Component {
+  render() {
+   return (
+     <View style={styles.container}>
+       <Form
+         ref={c => this._form = c}
+         type={Teacher}
+         options={options} // pass the options via props
+       />
+       <Button
+         title="Criar professor!"
+         onPress={this.handleSubmit}
+       />
+     </View>
+   );
+ }
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 50,
+    padding: 20,
+    backgroundColor: '#ffffff',
   },
 });
