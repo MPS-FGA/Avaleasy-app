0d0aa921040859dd8c2308346f48f3f9ebbebe4a
Lucas <lucasoliveira2306@gmail.com>
2018-09-24 21:58:09 -0300

    Improved login page style.
    Co-authored-by: Emanoel Belchior <belchior.emanoel@gmail.com>
    Signed-off-by: Lucas <lucasoliveira2306@gmail.com>

--- App.js
+++ App.js
@@ -1,23 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, Alert, Image } from 'react-native';
+
+import { Wallpaper } from "./login/background";
 
 export default class Login extends React.Component {
   _onPressEnter(){
     Alert.alert('You tapped the button!')
   }
+
+  _signUp() {
+    Alert.alert('You will can sign up soon!')
+  }
+
   constructor(props) {
     super(props);
-    this.state = { email: '', password: '' };
+    this.state = { email: 'Email', password: '' };
   }
+
   render() {
     return (
       <View style={styles.container}>
-        <TextInput onChangeText={(email) => this.setState({email})}
-        value={this.state.email} />
-        <TextInput secureTextEntry={true} onChangeText={(password) => this.setState({password})}
-        value={this.state.password} />
-        <Button onPress={this._onPressEnter} title="ENTRAR"
-        />
+        <Image style={styles.image} source={require('./images/wallpaper.png')}/>
+
+
+          <TextInput style={styles.textInput} onChangeText={(email) => this.setState({email})}
+          value={this.state.email} />
+          <TextInput style={styles.textInput} secureTextEntry={true} onChangeText={(password) => this.setState({password})}
+          value={this.state.password} />
+
+          <Button styles={styles.button} onPress={this._onPressEnter} title="ENTRAR" />
+          <Button styles={styles.button} onPress={this._signUp} title="CADASTRA-SE" />
+      </View>
     );
   }
 }
@@ -26,7 +39,23 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+  },
+
+  image: {
+    marginTop: 50,
+    marginBottom: 50,
+    alignSelf: 'center',
+  },
+
+  textInput: {
+    fontSize: 16,
+    marginBottom: 30
+  },
+
+  button: {
+    marginTop: 50,
+    marginLeft: 30,
+    marginRight: 30,
   },
 });
