386cd754c6824d4e8110d99796b713047bfeea93
Alax Alves <alaxallves@gmail.com>
2018-10-09 15:14:43 -0300

    Includes form type and grade in examModel
    Signed-off-by: Alax Alves <alaxallves@gmail.com>

--- src/examModel/createExamModel.js
+++ src/examModel/createExamModel.js
@@ -1,15 +1,16 @@
-import { React, Component } from 'react'
-import { View, StyleSheet } from 'react-native'
-import t from 'tcomb-form-native'
+import { React, Component } from 'react';
+import { View, StyleSheet } from 'react-native';
+import t from 'tcomb-form-native';
 
-const Form = t.form.Form
+const Form = t.form.Form;
 
 const ExamModel = t.struct({
   name: t.String,
   description: t.String,
   nmbr_questions: t.Integer,
-  
-})
+  grade: t.Float,
+  test_type: t.type,
+});
 
 const formOptions = {
   fields: {
@@ -22,30 +23,44 @@ const formOptions = {
     nmbr_questions: {
       error: 'O modelo de avaliação deve possuir questões',
     },
-  }
-}
+    grade: {
+      error: 'O modelo de avaliação deve possuir nota',
+    },
+    test_type: {
+      error: 'O modelo de avaliação deve possuir tipo',
+    },
+  },
+};
 
 export default class CreateExamModel extends Component {
   handleSubmit = () => {
-    const value = this._form.getValue()
-    
+    const value = this._form.getValue();
+
     // FIXME:
     // baseUrl must be replaced with actual server url
-    let baseUrl = 'http://localhost:3000/'
+    const baseUrl = 'http://localhost:3000/';
 
-    fetch(baseUrl + '/examsTemplates/new', {
+    fetch(`${baseUrl}/examsTemplates/new`, {
       method: 'POST',
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(value)
+      body: JSON.stringify(value),
     })
-    .catch((err) => {
-      console.error(error)
-    })
+      .catch((err) => {
+        console.error(err);
+      });
   }
-  
+
+  const styles = StyleSheet.create({
+    container: {
+      justifyContent: 'center',
+      marginTop: 50,
+      padding: 20,
+      backgroundColor: '#fafafa',
+    },
+
   render() {
     return (
       <View style={styles.container}>
@@ -53,21 +68,14 @@ export default class CreateExamModel extends Component {
           type={ExamModel}
           ref={c => this._form = c}
           options={formOptions}
-          />
+        />
         <Button
           title="Criar modelo"
           onPress={this.handleSubmit}
-          />
+        />
       </View>
-    )
+    );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    marginTop: 50,
-    padding: 20,
-    backgroundColor: '#fafafa',
-  },
-})
+});
