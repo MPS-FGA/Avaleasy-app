31e8455cb19185bc46da37d9961939329e99a511
matheusbsilva <matheusbattista@hotmail.com>
2018-10-09 20:42:18 -0300

    Fix import on app.js

--- App.js
+++ App.js
@@ -1,10 +1,9 @@
-import React, { Component } from 'react';
 import { View, ScrollView } from 'react-native';
 import { createStackNavigator } from 'react-navigation';
 import t from 'tcomb-form-native';
 
 import Login from './src/login/Login';
-import {ButtonComponent} from "./src/components/ButtonComponent";
+import { ButtonComponent } from "./src/components/ButtonComponent";
 import TeacherForm from './src/components/teachers_form.js'
 import Profile from './src/profile/Profile';
 import ExamTemplateList from './src/examTemplate/examTemplateList'
